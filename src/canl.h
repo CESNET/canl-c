94553690c727362da5ed3ce9c9cce66984eb61c2
Daniel Kouřil <kouril@ics.muni.cz>
2012-01-12 08:21:13 +0000

    SSL/PKI specific calls moved to canl_ssl.h

--- src/canl.h
+++ src/canl.h
@@ -13,10 +13,6 @@ typedef void *canl_io_handler;
 typedef void *canl_ctx;
 typedef void *cred_handler;
 
-typedef void *canl_x509;
-typedef void *canl_stack_of_x509;
-typedef void *canl_pkey;
-
 typedef char (*canl_password_callback)(canl_ctx cc, void *userdata);
 
 canl_ctx canl_create_ctx();
@@ -40,11 +36,6 @@ canl_get_error_message(canl_ctx);
 int canl_get_error(canl_ctx cc, char ** reason);
 int canl_io_close(canl_ctx cc, canl_io_handler io);
 int canl_io_destroy(canl_ctx cc, canl_io_handler io);
-
-int canl_set_ctx_own_cert(canl_ctx cc, canl_x509 cert,
-        canl_stack_of_x509 chain, canl_pkey key);
-int canl_set_ctx_own_cert_file(canl_ctx cc, char *cert, char *key,
-        canl_password_callback cb, void *userdata);
 
 #ifdef __cplusplus
 }       
---
+++ src/canl_ssl.h
@@ -0,0 +1,21 @@
+#ifndef _CANL_SSL_H
+#define _CANL_SSL_H
+
+#include <canl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef char (*canl_password_callback)(canl_ctx cc, void *userdata);
+
+int canl_set_ctx_own_cert(canl_ctx cc, canl_x509 cert,
+        canl_stack_of_x509 chain, canl_pkey key);
+int canl_set_ctx_own_cert_file(canl_ctx cc, char *cert, char *key,
+        canl_password_callback cb, void *userdata);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
